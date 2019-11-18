8b2f8d48d33d15e53a69472d8f035acc62b2ac09
Carlos Canabal <ccanabalr@gmail.com>
2019-11-18 08:12:06 -0500

    Modificando Puerto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = 3800;
+var port = process.env.PORT || 3800;
 
 //conexion a la base de datos
 mongoose.Promise = global.Promise;
